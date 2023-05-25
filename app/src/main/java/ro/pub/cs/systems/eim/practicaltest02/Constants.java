307b8b8f48f315def39294e72702e2bddd5857da
Lucian-MihaiStan <stanlucianmihai@yahoo.com>
2023-05-25 12:59:44 +0300

    without 6 and 7 but i am fine

---
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/Constants.java
@@ -0,0 +1,8 @@
+package ro.pub.cs.systems.eim.practicaltest02;
+
+public interface Constants {
+    String TAG = "[PracticalTest02]";
+    String EMPTY_STRING = "";
+    boolean DEBUG = true;
+    String WEB_SERVICE_ADDRESS = "https://api.coindesk.com/v1/bpi/currentprice/%s.json";
+}
